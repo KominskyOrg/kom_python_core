8840650fda0aa5b2151b57690775cf9b0917abf8
jaredkominsky <kominskyjared@gmail.com>
2024-11-07 21:31:01 -0600

    [feat] Initialization of kom_python_core

--- python_core/__init__.py
+++ python_core/__init__.py
@@ -1,13 +1,12 @@
 # python_core/__init__.py
 
 from .logging import configure_logging
-from .error_handler import configure_exception_handling
 from .middleware import correlation_id_middleware
 from .tracing import configure_tracing
+from .http_client import HTTPClient
 
 
 def initialize(app):
     configure_logging()
-    configure_exception_handling()
     correlation_id_middleware(app)
     configure_tracing(app)
