50b334289aee87d0b7afa6121663c44a72a2d8dc
jaredkominsky <kominskyjared@gmail.com>
2024-11-07 20:49:29 -0600

    [feat] Initialization of kom_python_core
    - adding setup.py for package

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kom_python_core',
+    version='0.1.0',
+    description='Core centralized packages for KominskyOrg Python utilities.',
+    author='Jared Kominsky',
+    author_email='kominskyjared@gmail.com',
+    url='https://github.com/kominskyorg/kom_python_core',
+    packages=find_packages(include=['python_core', 'python_core.*']),
+    install_requires=[
+        'flask',
+        'opentelemetry-sdk',
+        'opentelemetry-api',
+        'opentelemetry-instrumentation-flask',
+        'opentelemetry-exporter-otlp',
+    ],
+    extras_require={
+        'dev': [
+            'ruff',
+            'pytest',
+            'pytest-cov',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3.12',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.12',
+    include_package_data=True,
+    zip_safe=False,
+)
