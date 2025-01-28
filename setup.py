e44f4e60ffe464244792d26a69937ede7383c462
jaredkominsky <kominskyjared@gmail.com>
2025-01-27 21:25:09 -0600

    [feat] Update package version to 0.1.8
    - Updated package version from 0.1.7 to 0.1.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kom_python_core',
-    version='0.1.7',
+    version='0.1.8',
     description='Core centralized packages for KominskyOrg Python utilities.',
     author='Jared Kominsky',
     author_email='kominskyjared@gmail.com',
